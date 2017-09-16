63d6224dd8dfe26b2b9775e86d95713a5d68e5af
philippwalter <walter@abss.at>
2017-09-16 19:36:31 +0200

    configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,22 @@
 module.exports = {
-    "extends": "airbnb",
-    "plugins": ["react"],
-    "env": {
-        "browser": true,
-        "node": true
-    },
-    "globals": {
-        "React": true
-    },
-    "rules": {
-        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-        "semi": ["error", "never"],
-    }
-};
+  extends: 'airbnb',
+  plugins: ['react'],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  globals: {
+    React: true,
+  },
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    semi: ['error', 'never'],
+    "jsx-a11y/href-no-hash": "off",
+    "jsx-a11y/anchor-is-valid": ["warn", { "aspects": ["invalidHref"] }]
+  },
+  settings: {
+    // These packages are provided 'magically' by Gatsby
+    'import/core-modules': ['react', 'config', 'prop-types', 'react-helmet'],
+  },
+}
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Philipp Walter`,
+    title: 'Philipp Walter',
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: ['gatsby-plugin-react-helmet'],
 }
