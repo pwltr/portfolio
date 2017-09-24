537cb75c386db4bf05339b1238777b906433bcff
philippwalter <walter@abss.at>
2017-09-24 19:46:55 +0200

    update layout

---
+++ src/layouts/Content.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import Link from 'gatsby-link'
+import NavBar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+class Content extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      test: 'test'
+    }
+  }
+
+  render() {
+    return (
+      <div id="wrapper" className="wrapper">
+        <NavBar />
+        <div id="content" className="content-container">
+          <div className="main-container">
+            {this.props.children}
+          </div>
+          <Footer />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Content
