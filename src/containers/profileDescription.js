f2cd1cea3001a3c6c9e5151440dbe822a90ddb6c
Philipp Walter <philipp.walter2@gmail.com>
2019-02-07 19:33:27 +0100

    add profile-callout, add rudimentary resume page

--- src/containers/profileDescription.js
+++ src/containers/profileDescription.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { withPrefix } from 'gatsby'
 
 const ProfileDescription = () => (
   <div className="section section--profileDescription">
@@ -10,10 +11,15 @@ const ProfileDescription = () => (
       </div>
 
       <p className="section-text">
-        Mentoring and building quality code
-        <br />
-        Take a look at my skills below.
+      Fullstack JavaScript Developer mit Leidenschaft für die Entwicklung und dem Erlernen von Technologien. Ich bin in der Lage ein breites Spektrum an Lösungen umzusetzen und bereit eigenverantwortlich und aktiv zu handeln. Teamarbeit und der gemeinsame Austausch von Ideen sind für mich bei jedem Projekt Vorraussetzung.
+        <br /><br />
       </p>
+
+      <div className="profile-callout">
+        <div className="profile-co__text">Take a look at my skills below</div>
+        <div className="profile-co__delimiter">or maybe</div>
+        <a className="profile-co__btn" href={withPrefix('/cv-philippwalter.pdf')}>Download my resume</a>
+      </div>
     </div>
   </div>
 )
