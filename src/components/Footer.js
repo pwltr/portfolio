82c73a72c476456209c4f3fef58fce4be50b625b
Philipp Walter <philipp.walter2@gmail.com>
2019-02-07 23:10:39 +0100

    add logo to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,33 +5,38 @@ const toggleNightMode = () => document.body.classList.toggle('theme--dark')
 
 const NavBar = () => (
   <div className="footer">
-    <div className="social-links">
-      <a
-        className="social-link social-github"
-        href="https://github.com/backdot"
-        target="_blank">
-        <FaGithub />
-      </a>
-      <a
-        className="social-link social-linkedin"
-        href="https://linkedin.com/in/philipp-walter"
-        target="_blank">
-        <FaLinkedin />
-      </a>
-      <a
-        className="social-link social-twitter"
-        href="https://twitter.com/@_backdot"
-        target="_blank">
-        <FaTwitter />
-      </a>
+    <div className="footer-logo">
+      <img src="/logo.png" />
     </div>
-    <button
-      className="btn-darkMode"
-      onClick={toggleNightMode}>
-      <FaRegLightbulb className="lightbulb--off" />
-      <FaLightbulb className="lightbulb--on" />
-      Toggle Day/Night
-    </button>
+    <div className="footer-content">
+      <div className="social-links">
+        <a
+          className="social-link social-github"
+          href="https://github.com/backdot"
+          target="_blank">
+          <FaGithub />
+        </a>
+        <a
+          className="social-link social-linkedin"
+          href="https://linkedin.com/in/philipp-walter"
+          target="_blank">
+          <FaLinkedin />
+        </a>
+        <a
+          className="social-link social-twitter"
+          href="https://twitter.com/@_backdot"
+          target="_blank">
+          <FaTwitter />
+        </a>
+      </div>
+      <button
+        className="btn-darkMode"
+        onClick={toggleNightMode}>
+        <FaRegLightbulb className="lightbulb--off" />
+        <FaLightbulb className="lightbulb--on" />
+        Toggle Day/Night
+      </button>
+    </div>
   </div>
 )
 
