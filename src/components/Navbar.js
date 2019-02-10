ad38897659a1488aeda716e512de43afa260524a
Philipp Walter <philipp.walter2@gmail.com>
2019-02-10 21:15:48 +0100

    fix sticky nav style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,59 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { withPrefix } from 'gatsby'
 import Link from 'gatsby-link'
 import { MdMenu } from 'react-icons/md'
 import { bubble as Menu } from 'react-burger-menu'
+import stickybits from 'stickybits'
 
-const NavBar = () => (
-  <div className="navbar">
-    <div className="nav-desktop container">
-      <Link
-        to="/"
-        className="menu-item"
-        activeClassName="menu-item--active">
-        Introduction
-      </Link>
-      <Link
-        to="/profile/"
-        className="menu-item"
-        activeClassName="menu-item--active">
-        About Me
-      </Link>
-      <a
-        className="menu-item"
-        href={withPrefix('/cv-philippwalter.pdf')}
-        target="_blank"
-        rel="noopener">
-        Resume
-      </a>
-      {/* <Link
-        to="/work/"
-        className="menu-item"
-        activeClassName="menu-item--active">
-        Work
-      </Link> */}
-      <Link
-        to="/blog/"
-        className="menu-item"
-        activeClassName="menu-item--active">
-        Blog
-      </Link>
-      <Link
-        to="/contact/"
-        className="menu-item menu-item--has-btn"
-        activeClassName="menu-item--active">
-        <button className="btn">Hire Me</button>
-      </Link>
-    </div>
+const NavBar = () => {
+  useEffect(() => {
+    stickybits('.navbar', { useStickyClasses: true })
+  })
 
-    <div className="nav-mobile container">
-      <Menu
-        right
-        isOpen={false}
-        customBurgerIcon={<MdMenu className="menu-toggle" />}
-        pageWrapId={'content'}
-        outerContainerId={'wrapper'}
-      >
+  return (
+    <div className="navbar">
+      <div className="nav-desktop container">
         <Link
           to="/"
           className="menu-item"
@@ -66,12 +25,19 @@ const NavBar = () => (
           activeClassName="menu-item--active">
           About Me
         </Link>
-        <Link
+        <a
+          className="menu-item"
+          href={withPrefix('/cv-philippwalter.pdf')}
+          target="_blank"
+          rel="noopener">
+          Resume
+        </a>
+        {/* <Link
           to="/work/"
           className="menu-item"
           activeClassName="menu-item--active">
           Work
-        </Link>
+        </Link> */}
         <Link
           to="/blog/"
           className="menu-item"
@@ -80,13 +46,54 @@ const NavBar = () => (
         </Link>
         <Link
           to="/contact/"
-          className="menu-item"
+          className="menu-item menu-item--has-btn"
           activeClassName="menu-item--active">
-          Contact
+          <button className="btn">Hire Me</button>
         </Link>
-      </Menu>
+      </div>
+  
+      <div className="nav-mobile container">
+        <Menu
+          right
+          isOpen={false}
+          customBurgerIcon={<MdMenu className="menu-toggle" />}
+          pageWrapId={'content'}
+          outerContainerId={'wrapper'}
+        >
+          <Link
+            to="/"
+            className="menu-item"
+            activeClassName="menu-item--active">
+            Introduction
+          </Link>
+          <Link
+            to="/profile/"
+            className="menu-item"
+            activeClassName="menu-item--active">
+            About Me
+          </Link>
+          {/* <Link
+            to="/work/"
+            className="menu-item"
+            activeClassName="menu-item--active">
+            Work
+          </Link> */}
+          <Link
+            to="/blog/"
+            className="menu-item"
+            activeClassName="menu-item--active">
+            Blog
+          </Link>
+          <Link
+            to="/contact/"
+            className="menu-item"
+            activeClassName="menu-item--active">
+            Contact
+          </Link>
+        </Menu>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default NavBar
