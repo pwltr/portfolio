565d318ecbdec22fa9c5d6c0017edb834b8addc3
Philipp Walter <philipp.walter2@gmail.com>
2019-02-11 21:24:41 +0100

    fix link styles

--- src/containers/introduction.js
+++ src/containers/introduction.js
@@ -10,17 +10,19 @@ const Introduction = () => (
         </h2>
       </div>
 
-      <p className="section-text">
-        Hi, I'm Phil! 👋
-        <br />
-        I'm a JavaScript Developer from Vienna and I love using the web and building on top of it.
-        <br /><br />
-        At the moment I am <b>looking for a full-time position in Vienna</b> so
-        if you want to get in touch, write me an <a href="mailto:philippwalter@pm.me">email</a> or find me on <a href="https://twitter.com/@_backdot" target="_blank" rel="noopener">twitter</a>.
-        <br /><br />
-        {/* Feel free to take a look at <Link to="/work/">my projects</Link> or find out more <Link to="/profile/">about me.</Link> */}
-        Feel free to take a look at my projects or find out more <Link to="/profile/">about me.</Link>
-      </p>
+      <div className="section-text">
+        <p>
+          Hi, I'm Phil! 👋
+          <br />
+          I'm a JavaScript Developer from Vienna and I love using the web and building on top of it.
+          <br /><br />
+          At the moment I am <b>looking for a full-time position in Vienna</b> so
+          if you want to get in touch, write me an <a href="mailto:philippwalter@pm.me">email</a> or find me on <a href="https://twitter.com/@_backdot" target="_blank" rel="noopener">twitter</a>.
+          <br /><br />
+          {/* Feel free to take a look at <Link to="/work/">my projects</Link> or find out more <Link to="/profile/">about me.</Link> */}
+          Feel free to take a look at my projects or find out more <Link to="/profile/">about me.</Link>
+        </p>
+      </div>
     </div>
   </div>
 )
